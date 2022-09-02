de555cf3490942be94694110dba531164f7750fa
Malek Farhat <malek.f.94@gmail.com>
2022-09-02 14:57:19 +0100

    Changed app_id, region and authkey in consts

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const COMETCHAT_CONSTANTS = {
-	APP_ID: 'APP_ID',
-	REGION: 'REGION',
-	AUTH_KEY: 'AUTH_KEY'
-}
+	APP_ID: "218018eaf2dcee4f",
+	REGION: "eu",
+	AUTH_KEY: "e1aa9bce0000c4474261884d7bce10de54aaaf69",
+};
